04ee26cdd55b7f3399bed9d5861bf5270cf2e639
Flav <flavian.feugueur@epitech.eu>
2019-10-29 11:43:26 +0100

    adding the total of all epitech for the given module

--- grades.js
+++ grades.js
@@ -1,4 +1,4 @@
-const login = ''; // Put your autologin here
+const login = '0f52672c1c1dafd1808fb4d42a46e56da3182807'; // Put your autologin here
 
 if (login == '') {
     console.log('Missing autologin url.\nOpen the script and put him into login var');
@@ -15,10 +15,15 @@ const year = process.argv[2];
 const course = process.argv[3];
 const location = process.argv[4];
 
+const sem = '-' + location.split('-')[1] + '-' + location.split('-')[2];
+
 const url = 'https://intra.epitech.eu/auth-' + login + '/module/' + year + '/' + course + '/' + location + '/registered/?format=json';
 
+const city = ['REN', 'BDX', 'LIL', 'LYN', 'MAR', 'MPL', 'NCY', 'NAN', 'NCE', 'PAR', 'STG', 'TLS', 'TIR', 'BRU', 'COT', 'RUN', 'BER', 'BAR'];
+let allGrades = {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'Echec': 0};
 let grades = {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'Echec': 0};
 let total = 0;
+let totalNation = 1;
 let request;
 
 try {
@@ -45,4 +50,36 @@ request.get(url, (err, res, body) => {
     console.log('C: ' + grades['C'] + ' (' + (100 * grades['C'] / total).toFixed(2) + '%)');
     console.log('D: ' + grades['D'] + ' (' + (100 * grades['D'] / total).toFixed(2) + '%)');
     console.log('E: ' + grades['Echec'] + ' (' + (100 * grades['Echec'] / total).toFixed(2) + '%)');
-})
+});
+
+var accountfn = (elem) => {
+    return new Promise((resolve) => {
+        allGrades[elem.grade]++;
+        totalNation++;
+        resolve();
+    })
+}
+
+var fn = (elem) => { 
+    return new Promise((resolve, reject) => {
+        let url = 'https://intra.epitech.eu/auth-' + login + '/module/' + year + '/' + course + '/' + elem + sem + '/registered/?format=json';
+        request.get(url, (err, res, body) => {
+            const accounts = JSON.parse(body);
+            Promise.all(Object.values(accounts).map(accountfn)).then(() => {
+                resolve();
+            }).catch((error) => {reject(error)});;
+        })
+    });
+};
+
+Promise.all(city.map(fn)).then(() => {
+    console.log('national:');
+    console.log('Total: ' + totalNation + ' students');
+    console.log('A: ' + allGrades['A'] + ' (' + (100 * allGrades['A'] / totalNation).toFixed(2) + '%)');
+    console.log('B: ' + allGrades['B'] + ' (' + (100 * allGrades['B'] / totalNation).toFixed(2) + '%)');
+    console.log('C: ' + allGrades['C'] + ' (' + (100 * allGrades['C'] / totalNation).toFixed(2) + '%)');
+    console.log('D: ' + allGrades['D'] + ' (' + (100 * allGrades['D'] / totalNation).toFixed(2) + '%)');
+    console.log('E: ' + allGrades['Echec'] + ' (' + (100 * allGrades['Echec'] / totalNation).toFixed(2) + '%)');
+}).catch((e) => {
+    //console.log(e)
+});
